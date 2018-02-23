49cd5551990a4373c10805a039d7c3025c5628d5
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-23 15:09:33 +0100

    removes old and failing test

--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -26,31 +26,10 @@ public class HtmlTextConverterTest {
 
     @Test
     public void returns_converted_html_from_a_file_with_some_text() throws IOException {
-        HtmlTextConverter converter = new FullHtmlTextConverter("full");
-
-        assertEquals("first line<br />second &quot;line&quot;<br />", converter.convertToHtml());
-    }
-
-    @Test
-    public void new_returns_converted_html_from_a_file_with_some_text() throws IOException {
         TextReader reader = new FullTextReader();
         HtmlTextConverter converter = new HtmlTextConverter(reader);
 
         assertEquals("first line<br />second &quot;line&quot;<br />", converter.convertToHtml());
-    }
-
-    private class FullHtmlTextConverter extends HtmlTextConverter {
-        public FullHtmlTextConverter(String path) {
-            super(path);
-        }
-
-        @Override
-        protected List<String> readLines() throws IOException {
-            ArrayList<String> lines = new ArrayList<>();
-            lines.add("first line");
-            lines.add("second 'line'");
-            return lines;
-        }
     }
 
     private class EmptyTextReader extends TextReader {
