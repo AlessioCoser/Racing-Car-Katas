6f647080cc0ec53b7034cf1fc8b4ff4e21e4b66f
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-23 11:48:36 +0100

    adds test for empty file

--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -4,10 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class HtmlTextConverterTest {
     @Test
-    public void foo() {
+    public void returns_filename() {
         HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+
+        assertEquals("foo", converter.getFilename());
+    }
+
+    @Test
+    public void returns_empty_string_from_an_empty_file() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter("empty");
+
+        assertEquals("", converter.convertToHtml());
     }
 }
