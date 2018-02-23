b6c9e7686293fef105e6ef0390409a8d354a656b
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-23 17:05:38 +0100

    adds first fake test

---
+++ Java/TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -0,0 +1,12 @@
+package tddmicroexercises.turnticketdispenser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TicketDispenserTest {
+    @Test
+    public void XXX() {
+        assertTrue(true);
+    }
+}
