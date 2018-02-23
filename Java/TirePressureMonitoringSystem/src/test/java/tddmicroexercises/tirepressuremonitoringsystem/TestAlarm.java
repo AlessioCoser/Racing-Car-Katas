3a7d063a9a7ab4b74dfa41a4ab09dc37864d0f3c
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-23 10:08:19 +0100

    creates failing test

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -7,8 +7,17 @@ import static org.junit.Assert.*;
 public class TestAlarm {
 
     @Test
-    public void foo() {
+    public void is_off_after_initialization() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
+    }
+
+    @Test
+    public void is_off_when_pressure_within_threshold() {
+        Alarm alarm = new Alarm();
+
+        alarm.check();
+
+        assertFalse(alarm.isAlarmOn());
     }
 }
